50a87e208283256ec055fba1394443fd5d62eecb
naraynabhay <abhaynarayan07062002@gmail.com>
2024-06-16 21:56:59 +0530

    corsadded

--- app.js
+++ app.js
@@ -20,17 +20,11 @@ const allowedOrigins = [
    
 ];
 
-// CORS middleware
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}))
 
 // Route splitting
 app.use("/api/data", reportRouter);
